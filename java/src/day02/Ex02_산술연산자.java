e65e7ed86bbd8361d8126b8942fe95dfa0d5ca07
cheshirexlxl <cheshire.xlxl@gmail.com>
2025-10-23 18:08:14 +0900

    ~4일차

---
+++ java/src/day02/Ex02_산술연산자.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class Ex02_산술연산자 {
+
+	public static void main(String[] args) {
+		// 산술 연산자
+		System.out.println(10 + 5);
+		System.out.println(8 - 3);
+		System.out.println(5 * 4);
+		System.out.println(16 / 2);
+		System.out.println();
+		
+		System.out.println(10 % 6);		// 1. 4	--> 10을 6개씩 묶으면 4가 남음	
+		System.out.println(1 % 5);		// 2. 1 --> 1을 5개씩 묶으면 1
+		System.out.println(2 % 5);		// 3. 2 --> 2를 5개씩 묶으면 2
+		System.out.println(5 % 5);		// 4. 0 --> 5를 5개씩 묶으면 0
+		System.out.println();
+	}
+	
+}
